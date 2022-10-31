a0442eb85e8d109c1a4d9638b5fa8b8b9f14d6ab
Rog319 <al18760466@ite.edu.mx>
2022-10-31 13:39:13 -0700

    creacion seccion inventario

--- pages/inventario.js
+++ pages/inventario.js
@@ -1,9 +1,20 @@
-import Layout from "../components/Layout";
+import { ActionIcon, Title } from '@mantine/core';
+import Layout from '../components/Layout';
+import { IconPlus } from '@tabler/icons';
+import ListaInventario from '../components/ListaInventario';
 
 const Inventario = () => {
   return (
     <Layout>
-      <div>Inventario</div>
+      <div>
+        <Title align='center'>Lista de productos</Title>
+        <div>
+          <ActionIcon variant='filled' color='#01497c'>
+            <IconPlus size={18} />
+          </ActionIcon>
+          <ListaInventario />
+        </div>
+      </div>
     </Layout>
   );
 };
