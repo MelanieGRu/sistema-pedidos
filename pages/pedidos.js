06cf741174c2a5cddf4287377aaca99fa5760282
Rog319 <al18760466@ite.edu.mx>
2022-11-15 19:25:44 -0800

    cambios en pedidos

--- pages/pedidos.js
+++ pages/pedidos.js
@@ -1,12 +1,12 @@
-import Layout from "../components/Layout";
-import Pedido from "../components/Pedido";
-import styles from "../styles/Pedido.module.css";
+import Layout from '../components/Layout';
+import Pedido from '../components/Pedido';
+import styles from '../styles/Pedido.module.css';
 
 const Pedidos = ({ datos }) => {
   if (datos.length == 0) {
     return (
       <Layout>
-        <h1 className={styles.titulo}>No Existen Pedidos</h1>
+        <h1 className={styles.titulo}>Aun no tiene pedidos</h1>
       </Layout>
     );
   }
@@ -15,55 +15,55 @@ const Pedidos = ({ datos }) => {
     <Layout>
       {!datosimage.png
         ? null
-        : datos.map((dato) => <Pedido datos={dato} key={dato["id"]} />)}
+        : datos.map((dato) => <Pedido datos={dato} key={dato['id']} />)}
     </Layout>
   );
 };
 
 export default Pedidos;
 export async function getStaticProps() {
-  const res = await fetch("http://localhost:1337/pedidos");
+  const res = await fetch('http://localhost:1337/pedidos');
   const pedidos = await res.json();
 
   let datos = [];
 
   for (const pedido of pedidos) {
     const resUsuario = await fetch(
-      "http://localhost:1337/usuarios/" + pedido["usuario_id"]
+      'http://localhost:1337/usuarios/' + pedido['usuario_id']
     );
     const usuario = await resUsuario.json();
 
-    const resPedidoLineas = await fetch("http://localhost:1337/pedido-lineas");
+    const resPedidoLineas = await fetch('http://localhost:1337/pedido-lineas');
     const pedidoLineas = await resPedidoLineas.json();
 
     let lineas = [];
     for (const linea of pedidoLineas) {
-      if (pedido["id"] == linea["pedido_id"]) {
+      if (pedido['id'] == linea['pedido_id']) {
         lineas.push(linea);
       }
     }
 
     let productos = {};
-    const resProductos = await fetch("http://localhost:1337/productos");
+    const resProductos = await fetch('http://localhost:1337/productos');
     const productosRegresados = await resProductos.json();
 
     for (const producto of productosRegresados) {
-      productos[`${producto["id"]}`] = producto["nombre"];
+      productos[`${producto['id']}`] = producto['nombre'];
     }
 
     datos.push({
-      id: pedido["id"],
-      fecha: pedido["published_at"],
-      nombre: usuario["nombre"],
-      correo: usuario["correo"],
-      telefono: usuario["telefono"],
-      perfil: usuario["perfil"],
-      estado: pedido["estado"],
+      id: pedido['id'],
+      fecha: pedido['published_at'],
+      nombre: usuario['nombre'],
+      correo: usuario['correo'],
+      telefono: usuario['telefono'],
+      perfil: usuario['perfil'],
+      estado: pedido['estado'],
       lineas: lineas,
-      comentario: pedido["comentario"],
+      comentario: pedido['comentario'],
       productos: productos,
-      respuesta: pedido["respuesta"],
-      titulo: pedido["titulo"],
+      respuesta: pedido['respuesta'],
+      titulo: pedido['titulo'],
     });
   }
 
